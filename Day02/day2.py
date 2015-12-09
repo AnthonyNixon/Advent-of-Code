e598a5d2552c68b8e13fe6f79b0cfd634db02343
Anthony Nixon <ajn0592@gmail.com>
2015-12-08 23:09:04 -0600

    Add Day 2 part 2

--- Day02/day2.py
+++ Day02/day2.py
@@ -4,6 +4,7 @@ with open("input.txt") as f:
     content = f.readlines()
 	
 totalNeeded = 0
+totalRibbon = 0
 
 for i in range(0, len(content)):
 	line = content[i]
@@ -19,7 +20,12 @@ for i in range(0, len(content)):
 		print "2*" + str(combination[0] * combination[1]) + " (" + str(combination[0]) + "*" + str(combination[1]) + ")\t",
 		comboNeeded += 2 * combination[0] * combination[1]
 	comboNeeded += combinations[0][0] * combinations[0][1]
-	print "\t=" + str(comboNeeded) + "\n"
+	
+	ribbonNeeded = ((parts[0] * 2) + (parts[1] * 2))
+	ribbonNeeded += (parts[0] * parts[1] * parts[2])
+	
+	print "\t=" + str(comboNeeded) + "\tRibbon=" + str(ribbonNeeded) + "\n"
 	totalNeeded += comboNeeded
+	totalRibbon += ribbonNeeded
 
-print str(totalNeeded)
+print str(totalNeeded) + ", ribbon = " + str(totalRibbon)
