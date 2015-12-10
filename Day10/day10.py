58b04e86396d130a150323f275ed102b1c875028
Anthony Nixon <ajn0592@gmail.com>
2015-12-10 00:17:49 -0600

    Making solution shorter

--- Day10/day10.py
+++ Day10/day10.py
@@ -1,7 +1,4 @@
 import sys
-import itertools
-import re
-import json
 
 if len(sys.argv) == 2:
 	filename = sys.argv[1] + ".txt"
@@ -11,35 +8,22 @@ else:
 with open (filename, "r") as f:
     content = f.read().replace("\n", '')
 
-
 def playGame(string):
     currentChar = string[:1]
     count = 1
     returnString = ""
-
-    if len(string) > 1:
-        for i in range(1, (len(string))):
-            if not string[i] == currentChar:
-                returnString += str(count) + str(currentChar)
-                currentChar = string[i]
-                count = 1
-            else:
-                count += 1
+    for i in range(1, (len(string))):
+        if not string[i] == currentChar:
+            returnString += str(count) + str(currentChar)
+            currentChar = string[i]
+            count = 1
+        else:
+            count += 1
 
     returnString += str(count) + str(currentChar)
     return returnString
 
+for i in range(0, 50):
+    content = playGame(content)
 
-
-
-numIterations = 50
-
-string = content
-
-for i in range(0, numIterations):
-    # print string
-    # print
-    string = playGame(string)
-
-print string
-print len(string)
+print len(content)
