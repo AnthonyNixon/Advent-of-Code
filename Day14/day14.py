d6e39c31dd32784a7e35545a4ad341a9fe0d0355
Anthony Nixon <Anthony.Nixon@target.com>
2015-12-14 09:28:32 -0600

    Add final version of Day 14

--- Day14/day14.py
+++ Day14/day14.py
@@ -3,8 +3,15 @@ import itertools
 import re
 import json
 
+
+simSeconds = 0
 if len(sys.argv) == 2:
-	filename = sys.argv[1] + ".txt"
+	simSeconds = int(sys.argv[1])
+	filename = "input.txt"
+
+elif len(sys.argv) == 3:
+	simSeconds = int(sys.argv[1])
+	filename = sys.argv[2] + ".txt"
 else:
 	filename = "input.txt"
 
@@ -14,30 +21,79 @@ with open (filename, "r") as f:
 reindeer = {}
 
 for line in content:
-    matchObj = re.match(r'(.+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+)', line)
-    deer = matchObj.group(1)
-    speed = matchObj.group(2)
-    flyTime = matchObj.group(3)
-    restTime = matchObj.group(4)
+	matchObj = re.match(r'(.+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+)', line)
+	deer = matchObj.group(1)
+	speed = matchObj.group(2)
+	flyTime = matchObj.group(3)
+	restTime = matchObj.group(4)
 
-    reindeer[deer] = {}
-    reindeer[deer]['speed'] = speed
-    reindeer[deer]['flyTime'] = flyTime
-    reindeer[deer]['restTime'] = restTime
-    reindeer[deer]['distance'] = 0
-    reindeer[deer]['restingAtTime'] = -1
+	reindeer[deer] = {}
+	reindeer[deer]['speed'] = int(speed)
+	reindeer[deer]['flyTime'] = int(flyTime)
+	reindeer[deer]['restTime'] = int(restTime)
+	reindeer[deer]['distance'] = 0
+	reindeer[deer]['restingAtTime'] = 0
+	reindeer[deer]['state'] = {'currently': 'flying', 'since': 0}
+	reindeer[deer]['winPoints'] = 0
 
-def simulateDeer(time):
-    return 1
+def printReindeer():
+	for deer in reindeer:
+		print reindeer[deer]
+		print
+
+def printWinner(version):
+	if version == 'v1':
+		maxDist = 0
+		winner = ''
+		for deer in reindeer:
+			if reindeer[deer]['distance'] > maxDist:
+				maxDist = reindeer[deer]['distance']
+				winner = deer
+		print winner + " won at distance of " + str(maxDist)
+	elif version == 'v2':
+		maxPoints = 0
+		winner = ''
+		for deer in reindeer:
+			if reindeer[deer]['winPoints'] > maxPoints:
+				maxPoints = reindeer[deer]['winPoints']
+				winner = deer
+		print winner + " won with " + str(maxPoints) + " points"
+
+def getCurrentLeaders():
+	maxDist = 0
+	leaders = []
+	for deer in reindeer:
+		if reindeer[deer]['distance'] > maxDist:
+			maxDist = reindeer[deer]['distance']
+	for deer in reindeer:
+		if reindeer[deer]['distance'] == maxDist:
+			leaders.append(deer)
+	return leaders
+
+
+
+def simulateDeer(deer, time):
+	deer = reindeer[deer]
+	if deer['state']['currently'] == 'flying':
+		deer['distance'] += int(deer['speed'])
+		if time - deer['state']['since'] == deer['flyTime']:
+			deer['state'] = {'currently': 'resting', 'since': time}
+	elif deer['state']['currently'] == 'resting':
+		if time - deer['state']['since'] == deer['restTime']:
+			deer['state'] = {'currently': 'flying', 'since': time}
 
 def simulateRace(time):
-    currentTime = 0
-    while currentTime < time:
-        for deer in reindeer:
-            simulateDeer(currentTime)
-        currentTime += 1
-    print reindeer
+	currentTime = 1
+	while currentTime <= time:
+		print "Time = " + str(currentTime)
+		for deer in reindeer:
+			simulateDeer(deer, currentTime)
+		for deer in getCurrentLeaders():
+			reindeer[deer]['winPoints'] += 1
+		currentTime += 1
+	printReindeer()
+	printWinner('v1')
+	printWinner('v2')
 
 
-print reindeer
-simulateRace(2503)
+simulateRace(simSeconds)
