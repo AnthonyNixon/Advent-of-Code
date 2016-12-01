3e9ab1ae9cae1189a4482c000d94acd1195e4510
Anthony Nixon <Anthony.Nixon@target.com>
2016-12-01 10:05:02 -0600

    Add day 1 parts 1 and 2:

---
+++ 2016/Day01/day01part02.py
@@ -0,0 +1,39 @@
+import re
+
+# Read in the input file.
+with open ("input.txt", "r") as myfile:
+    data=myfile.read().replace('\n', '')
+
+# Split the data read in into a list delimited by commas.
+instructions = data.split(',')
+
+directions = [(0, 1), (1, 0), (0, -1), (-1, 0)] # Direction Vectors for North, South, East, WEst
+currentDirectionIndex = 0
+currentLocation = [0, 0]
+visitedLocations = []
+solved = False
+
+for instruction in instructions:
+    if not solved:
+        # Change the direction based on the instruction
+        if 'R' in instruction:
+            currentDirectionIndex += 1
+        else: currentDirectionIndex -= 1
+
+        # Fix out of bounds issues
+        if currentDirectionIndex < 0:
+            currentDirectionIndex = 3
+        elif currentDirectionIndex > 3:
+            currentDirectionIndex = 0
+
+
+        amount = re.findall(r'\d+', instruction)
+        amount = int(amount[0])
+
+        for i in range(amount):
+            currentLocation[0] += (directions[currentDirectionIndex][0])
+            currentLocation[1] += (directions[currentDirectionIndex][1])
+            if (currentLocation[0], currentLocation[1]) in visitedLocations:
+                print abs(currentLocation[0]) + abs(currentLocation[1])
+                solved = True
+            visitedLocations.append((currentLocation[0], currentLocation[1]))
